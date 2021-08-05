9c7f2cde35d466897abc6815f543ebfe1848131a
DESKTOP-1BM7939\mhmni <mhmn.inabox@gmail.com>
2021-08-05 17:19:44 +0800

    first

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.sdproject;
+package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
--- app/src/test/java/com/example/myapplication/ExampleUnitTest.java
+++ app/src/test/java/com/example/myapplication/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.sdproject;
+package com.example.myapplication;
 
 import org.junit.Test;
 
