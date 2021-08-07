63c95cb91de963c7e3dfc472d566a4e3d6b3ef5b
DESKTOP-1BM7939\mhmni <mhmn.inabox@gmail.com>
2021-08-07 12:32:05 +0800

    DESIGNED REGISTER SCREEN

--- app/src/main/java/com/example/myapplication/LoginScreen.java
+++ app/src/main/java/com/example/myapplication/LoginScreen.java
@@ -3,12 +3,24 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginScreen extends AppCompatActivity {
+
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
+        login = findViewById(R.id.btnlogin);
+        /*login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });*/
     }
 }
