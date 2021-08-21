0c878d2f69ec8488b35178a9e6083a19364ed19c
mhmnal <mhmn.inabox@gmail.com>
2021-08-21 17:01:40 +0800

    specifichabit
    specifichabit

--- app/src/main/java/com/example/habitfinal/MyAdapter.java
+++ app/src/main/java/com/example/habitfinal/MyAdapter.java
@@ -1,6 +1,7 @@
 package com.example.habitfinal;
 
 import android.content.Context;
+import android.content.Intent;
 import android.icu.text.Transliterator;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,6 +9,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -15,6 +17,7 @@ import java.util.ArrayList;
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
     Context context;
+
 
     ArrayList<cHabitInfo> list;
 
@@ -36,6 +39,8 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         cHabitInfo chabitInfo = list.get(position);
         holder.namehabittrv.setText(chabitInfo.getNameHabit());
 
+
+
     }
 
     @Override
@@ -51,6 +56,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
             super(itemView);
 
             namehabittrv = itemView.findViewById(R.id.namehabitrv);
+
         }
     }
 }
