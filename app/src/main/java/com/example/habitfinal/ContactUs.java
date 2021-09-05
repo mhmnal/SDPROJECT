05437e9b74dec63887dd36e810d570fc763ce062
hmznzaidi <hamizantaugey13@gmail.com>
2021-09-05 18:36:25 +0800

    Contact us page, ada no phone and email

--- app/src/main/java/com/example/habitfinal/ContactUs.java
+++ app/src/main/java/com/example/habitfinal/ContactUs.java
@@ -2,11 +2,39 @@ package com.example.habitfinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.net.Uri;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class ContactUs extends AppCompatActivity {
+
+    ImageButton androidImageButton;
+
+    Button buttonCall;
+
     @Override
-    protected void onCreate(Bundle saveInstanceState){
-        super.onCreate(saveInstanceState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
+
+        TextView email = (TextView) findViewById(R.id.tv_habitcom);
+        email.setText(Html.fromHtml("<a href=\"mailto:HabitSmart@gmail.com\">HabitSmart@gmail.com</a>"));
+        email.setMovementMethod(LinkMovementMethod.getInstance());
+
+
+        buttonCall = findViewById(R.id.btn_call1);
+        buttonCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:+60 13-317-4100"));
+                startActivity(intent);
+            }
+        });
     }
 }
